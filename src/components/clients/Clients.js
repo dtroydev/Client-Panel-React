c375be3c5b070001f37d93102692549e6fdf7b2d
Daniel <daniel@Daniels-Air.home.gateway>
2018-11-13 17:52:55 +1100

    Total Calculation. Fix: Client Email Display

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -2,17 +2,40 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { firestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
+import { firestoreConnect } from 'react-redux-firebase';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 
 class Clients extends Component {
+  state = {
+    totalOwed: 0,
+  }
+
+  static getDerivedStateFromProps({ clients }, state) {
+    if (clients === undefined) return state;
+    const totalOwed = clients
+      .reduce((total, { balance }) => total + parseFloat(balance), 0)
+      .toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+    return { totalOwed };
+  }
+
   render() {
-    if (this.props.clients) {
+    const { clients } = this.props;
+    const { totalOwed } = this.state;
+    if (clients) {
       return (
         <div>
           <div className="row">
-            <h2 className="col-md-12 text-center"><i className="fas fa-users"></i> Clients</h2>
+            <div className="col-md-6">
+              <h2><i className="fas fa-users"></i> Clients</h2>
+            </div>
+            <div className="col-md-6">
+              <h5 className="text-right text-secondary">Total Owed: <span className="text-primary">${totalOwed}</span>
+              </h5>
+            </div>
           </div>
           <table className="table table-striped">
             <thead className="thead-inverse">
@@ -24,10 +47,10 @@ class Clients extends Component {
               </tr>
             </thead>
             <tbody>
-              {this.props.clients.map(client => (
+              {clients.map(client => (
                 <tr key={client.id}>
                   <td>{client.firstName} {client.lastName}</td>
-                  <td>{client.lastName}</td>
+                  <td>{client.email}</td>
                   <td>${parseFloat(client.balance).toFixed(2)}</td>
                   <td><Link to={`/client/${client.id}`} className="btn btn-secondary btn-sm">
                     <i className="fas fa-arrow-circle-right"></i>Details
