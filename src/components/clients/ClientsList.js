2d0d0c7fd38b64cbed4d84f94e3f0c43a18e0c26
Daniel <daniel@Daniels-Air.home.gateway>
2018-11-28 15:02:11 +1100

    style: responsive clients table

--- src/components/clients/ClientsList.js
+++ src/components/clients/ClientsList.js
@@ -32,28 +32,30 @@ export default function ClientsList(props) {
           </h5>
         </div>
       </div>
-      <table className="table table-striped">
-        <thead className="thead-inverse">
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Balance</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {clients.map(client => (
-            <tr key={client.id}>
-              <td>{client.firstName} {client.lastName}</td>
-              <td>{client.email}</td>
-              <td className="text-right">${formatBalance(client.balance)}</td>
-              <td><Link to={`/client/${client.id}`} className="btn btn-secondary btn-sm">
-                <i className="fas fa-arrow-circle-right"></i>Details
-            </Link></td>
+      <div className="table-responsive">
+        <table className="table table-striped">
+          <thead className="thead-inverse">
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th className="text-center">Balance</th>
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {clients.map(client => (
+              <tr key={client.id}>
+                <td>{client.firstName} {client.lastName}</td>
+                <td>{client.email}</td>
+                <td className="text-right">${formatBalance(client.balance)}</td>
+                <td><Link to={`/client/${client.id}`} className="btn btn-secondary btn-sm">
+                  <i className="fas fa-arrow-circle-right"></i>Details
+            </Link></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div >
   );
 }
